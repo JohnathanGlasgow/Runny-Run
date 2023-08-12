55ca71db159d93194b6874e8f6514d85aef4dfff
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2023-08-12 22:20:29 +1200

    script: add player movement script

---
+++ RunnyRun/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	[SerializeField] private Rigidbody2D rb;
+	[SerializeField] private Transform GFX;
+	[SerializeField] private float jumpForce = 10f;
+	[SerializeField] private LayerMask groundLayer;
+	[SerializeField] private Transform feetPos;
+	[SerializeField] private float groundDistance = 0.25f;
+	[SerializeField] private float jumpTime = 0.3f;
+
+	[SerializeField] private float crouchHeight = 0.5f;
+
+	private bool isGrounded = false;
+	private bool isJumping = false;
+	private float jumpTimer;
+
+    private void Update()
+    {
+		isGrounded = Physics2D.OverlapCircle(feetPos.position, groundDistance, groundLayer);
+
+		#region JUMPING
+		if (isGrounded && Input.GetButtonDown("Jump")) {
+			isJumping = true;
+			rb.velocity = Vector2.up * jumpForce;
+		}
+
+		if (isJumping && Input.GetButton("Jump")) {
+
+			if (jumpTimer < jumpTime) {
+				rb.velocity = Vector2.up * jumpForce;
+
+				jumpTimer += Time.deltaTime;
+			} else {
+				isJumping = false;
+			}
+
+		}
+
+		if (Input.GetButtonUp("Jump")) {
+			isJumping = false;
+			jumpTimer = 0;
+		}
+		#endregion
+
+    }
+}

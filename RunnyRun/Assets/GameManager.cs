9e9ff4ddfeb38056025901a7678ffc669729d18e
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2023-08-29 09:16:30 +1200

    script: add GameManager

---
+++ RunnyRun/Assets/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
+    #endregion
+
+    public float currentScore = 0f;
+    public bool isPlaying = false;
+
+    private void Update()
+    {
+        if (isPlaying)
+        {
+            currentScore += Time.deltaTime;
+        }
+    }
+
+    public void GameOver()
+    {
+        currentScore = 0;
+    }
+    public string PrettyScore() => Mathf.RoundToInt(currentScore).ToString();
+
+}
+

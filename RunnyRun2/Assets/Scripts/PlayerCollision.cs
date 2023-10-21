82b3f8d0c55cda116dc5024f7f771927ea71fa04
JohnathanGlasgow <johnathanglasgow@gmail.com>
2023-10-21 23:46:02 +1300

    fix: fix player position before reset

--- RunnyRun2/Assets/Scripts/PlayerCollision.cs
+++ RunnyRun2/Assets/Scripts/PlayerCollision.cs
@@ -9,21 +9,38 @@ public class PlayerCollision : MonoBehaviour
 	/// It is attached to the Player game object.
 	/// </summary>
 	
+    [SerializeField] private Transform playerSpriteGroup;
+	private Vector3 initPosition;
 	private void Start() {
+		initPosition = transform.position;
 		GameManager.Instance.OnPlay.AddListener(activatePlayer);
 	}
 
 	private void activatePlayer() {
+		resetPlayer();
 		gameObject.SetActive(true);
+		
 	}
 
 	private void OnCollisionEnter2D(Collision2D other) {
 		if (other.transform.tag == "Obstacle") {
 			// log the collision
-			Debug.Log("Player collided with obstacle");
 			gameObject.SetActive(false);
             GameManager.Instance.GameOver();
 		}
 	}
+    private void resetPlayer()
+    {
+        // log out  the call
+        Debug.Log("resetPlayer called");
+        // set playerSpriteGroup.transform.rotation 0, 0, 0
+		// logout the current rotation
+		Debug.Log("current rotation: " + playerSpriteGroup.transform.localRotation);
+        playerSpriteGroup.localRotation = Quaternion.identity;
+		// logout the new rotation
+		Debug.Log("new rotation: " + playerSpriteGroup.transform.localRotation);
 
+		// set this.transform.position to initPosition
+		transform.position = initPosition;
+    }
 }

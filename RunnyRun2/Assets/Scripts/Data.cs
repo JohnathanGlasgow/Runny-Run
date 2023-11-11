c2aa13992a01f01e08d1de32fd82a38862245427
JohnathanGlasgow <johnathanglasgow@gmail.com>
2023-11-11 23:41:54 +1300

    docs: add comments

--- RunnyRun2/Assets/Scripts/Data.cs
+++ RunnyRun2/Assets/Scripts/Data.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// <summary>
 /// This class is used for storing any persistent data that needs to be saved between game sessions.
 /// This class is used in conjunction with the SaveSystem class.
-/// Currently it only stores the high score.
+/// This save system is not currently used in the game, but is left in for future use.
 /// </summary>
 [System.Serializable]
 public class Data
--- RunnyRun2/Assets/Scripts/DestroyOnTrigger.cs
+++ RunnyRun2/Assets/Scripts/DestroyOnTrigger.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class is used for destroying objects when they collide with a trigger.
+/// </summary>
+
 public class DestroyOnTrigger : MonoBehaviour
 {
+    // The tag of the object you want to destroy.
     public string tagFilter;
 
-
+    /// <summary>
+    /// This method is called when the object collides with a trigger.
+    /// </summary>
+    /// <param name="other">The collider of the object that collided with this object.</param>
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(tagFilter))
@@ -16,9 +24,9 @@ public class DestroyOnTrigger : MonoBehaviour
             {
                 Destroy(gameObject);
             }
+            // otherwise its an obstacle so the parent object needs to be destroyed
             else
             {
-                // destroy my parent object
                 Destroy(transform.parent.gameObject);
             }
         }
